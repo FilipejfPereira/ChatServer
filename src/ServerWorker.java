0b35d061935c861fe5f34c051510374f90f445e3
FilipejfPereira <filipejfpereira@gmail.com>
2019-06-30 17:07:38 +0100

    tried list command

--- src/ServerWorker.java
+++ src/ServerWorker.java
@@ -2,41 +2,57 @@ import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
 
 public class ServerWorker implements Runnable {
 
     private Socket clientSocket;
-    Client client;
-    String s;
+    private Client client;
+    private String messageToSend;
+    private String name;
     private boolean messageToSent = false;
-    DataOutputStream out;
+    private DataOutputStream out;
 
-    public ServerWorker(Socket server, Client client) {
+    public ServerWorker(Socket server, Client client, String name) {
         clientSocket = server;
         this.client = client;
+        this.name = name;
     }
 
     @Override
     public void run() {
         try {
+
             while (true) {
 
 
                 out = new DataOutputStream(clientSocket.getOutputStream());
                 BufferedReader bReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                out.writeBytes(name + " connected to server" + "\n");
                 //String msg = bReader.readLine();
                 String msg;
+
                 while((msg = bReader.readLine()) != null){
+
+
                         if(msg.equalsIgnoreCase("/quit")){
+                            out.writeBytes("You have been disconnected from server");
 
                             break;
                         }
-                        s = (msg + "\n");
-                        messageToSent = true;
+                        if(msg.contains("/alias")){
+                            messageToSend = msg;
+                            messageToSent = true;
+                        }else if(msg.contains("/list")){
+                            messageToSend = msg;
+                            messageToSent = true;
 
+
+                        } else {
+                            messageToSend = (msg + "\n");
+                            messageToSent = true;
+                        }
                 }
                 clientSocket.close();
             }
@@ -49,8 +65,8 @@ public class ServerWorker implements Runnable {
 
     }
 
-    public String getS() {
-        return s;
+    public String getMessageToSend() {
+        return messageToSend;
     }
 
     public boolean isMessageToSent() {
@@ -69,6 +85,13 @@ public class ServerWorker implements Runnable {
         return clientSocket;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
 
 }
